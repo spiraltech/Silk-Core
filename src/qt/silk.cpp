bffd31dea9b6ec11b3b9689c358fd23167f43d79
amirabrams <amirabrams@mail.com>
2016-09-19 01:17:08 -0500

    [Qt] Fix build issue for Windows

--- src/qt/silk.cpp
+++ src/qt/silk.cpp
@@ -51,24 +51,19 @@
 #include <QTimer>
 #include <QTranslator>
 
-#if defined(QT_STATICPLUGIN)
-#include <QtPlugin>
-#if QT_VERSION < 0x050000
-Q_IMPORT_PLUGIN(qcncodecs)
-Q_IMPORT_PLUGIN(qjpcodecs)
-Q_IMPORT_PLUGIN(qtwcodecs)
-Q_IMPORT_PLUGIN(qkrcodecs)
-Q_IMPORT_PLUGIN(qtaccessiblewidgets)
-#else
-Q_IMPORT_PLUGIN(AccessibleFactory)
-#if defined(QT_QPA_PLATFORM_XCB)
-Q_IMPORT_PLUGIN(QXcbIntegrationPlugin);
-#elif defined(QT_QPA_PLATFORM_WINDOWS)
-Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
-#elif defined(QT_QPA_PLATFORM_COCOA)
-Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
-#endif
-#endif
+#if defined(QT_STATICPLUGIN) && QT_VERSION < 0x050000
+    Q_IMPORT_PLUGIN(qcncodecs)
+    Q_IMPORT_PLUGIN(qjpcodecs)
+    Q_IMPORT_PLUGIN(qtwcodecs)
+    Q_IMPORT_PLUGIN(qkrcodecs)
+    Q_IMPORT_PLUGIN(qtaccessiblewidgets)
+    #if defined(QT_QPA_PLATFORM_XCB)
+        Q_IMPORT_PLUGIN(QXcbIntegrationPlugin);
+    #elif defined(QT_QPA_PLATFORM_WINDOWS)
+        Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+    #elif defined(QT_QPA_PLATFORM_COCOA)
+        Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
+    #endif
 #endif
 
 #if QT_VERSION < 0x050000
